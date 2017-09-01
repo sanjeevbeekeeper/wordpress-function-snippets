cf79f1a77ff438d48a3fea5181713c4def55a841
Sanjeev Beekeeper PC home <sanjeevramanathan@gmail.com>
2017-09-01 23:26:52 +0530

    Added the files from the inc folder.

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
-// functions.php
-// 01 Sep 2017
+/*
+ * functions.php
+ * Beekeeper Design Studio
+ * Created on 1st Sep 2017
+ * Main functions.php file
+*/
+
+require get_template_directory() . '/inc/remove_emoji.php';
+require get_template_directory() . '/inc/image_compression.php';
+require get_template_directory() . '/inc/disable_visual_editor.php';
+require get_template_directory() . '/inc/hide_version.php';
+require get_template_directory() . '/inc/remove_meta_generator.php';
 
  ?>
