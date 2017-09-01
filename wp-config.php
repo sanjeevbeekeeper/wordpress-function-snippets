c40cea51390af2d776ece3020cfb36d02123097d
Sanjeev Beekeeper PC home <sanjeevramanathan@gmail.com>
2017-09-01 23:47:57 +0530

    newly added

---
+++ wp-config.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Disable Editor
+ * Beekeeper Design Studio
+ * Added on 1st Sep 2017
+ */
+define( 'DISALLOW_FILE_EDIT', true );
+
+/*
+ * Autosave Interval
+ * Beekeeper Design Studio
+ * Added on 1st Sep 2017
+ */
+define('AUTOSAVE_INTERVAL', 120); // in seconds
+
+/*
+ * Post revision - limit
+ * Beekeeper Design Studio
+ * Added on 1st Sep 2017
+ */
+define('WP_POST_REVISIONS', 5); // number of revisions
+
+/*
+ * Post revision - disable
+ * Beekeeper Design Studio
+ * Added on 1st Sep 2017
+ */
+define('WP_POST_REVISIONS', false);
+
+/*
+ * Empty Trash in days, By default it is 30 days
+ * Beekeeper Design Studio
+ * Added on 1st Sep 2017
+ */
+define('EMPTY_TRASH_DAYS', 7 ); // Empty trash in amount of days
+
+/*
+ * Increase memory limit
+ * Beekeeper Design Studio
+ * Added on 1st Sep 2017
+ */
+define('WP_MEMORY_LIMIT', '64M'); // 64 mb
+
+/* That's all, stop editing! Happy blogging. */
+ ?>
