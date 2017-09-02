234000175471cff7e7fa903eeb217f70d7a36e55
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-09-02 18:30:10 +0530

    New inc files are added.

--- functions.php
+++ functions.php
@@ -6,10 +6,15 @@
  * Main functions.php file
 */
 
+// for Optimization
 require get_template_directory() . '/inc/remove_emoji.php';
 require get_template_directory() . '/inc/image_compression.php';
-require get_template_directory() . '/inc/disable_visual_editor.php';
 require get_template_directory() . '/inc/hide_version.php';
 require get_template_directory() . '/inc/remove_meta_generator.php';
 
- ?>
+// Optional
+require get_template_directory() . '/inc/disable_visual_editor.php';
+require get_template_directory() . '/inc/remove_dashboard_menus.php';
+require get_template_directory() . '/inc/show_user_id.php';
+// Custom Editing
+require get_template_directory() . '/inc/login_page.php'; // require CSS
